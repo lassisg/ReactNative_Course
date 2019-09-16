5eeee36d6790fca64def0a08cae4de815e661ab5
Leandro Gabriel <50912618+lassisg@users.noreply.github.com>
2019-09-16 14:49:47 +0100

    Commit inicial.

---
+++ hunt/src/routes.js
@@ -0,0 +1,13 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import Main from './pages/main';
+import Product from './pages/product';
+
+const AppNavigator = createStackNavigator({
+    Main,
+    Product
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default AppContainer;
